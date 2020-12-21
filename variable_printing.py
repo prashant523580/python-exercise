9be8c57f3c32a01f4fe6af60f1e8e7fa6cf1efd9
prashant523580 <prashant.thapa62758@gmail.com>
2020-12-21 23:16:42 +0545

    python variable printing

---
+++ variable_printing.py
@@ -0,0 +1,13 @@
+username = 'Zen A Shaw'
+userage = 23 #inch
+userheight = 53 #1bs
+userweight = 180
+usereye = 'Brown'
+userteeth = 'White'
+userhair = 'Black'
+
+print("Let's talk about %s." % username)
+print("He's %d insch tall. " % userheight)
+print("He's %d pound heavy." % userweight)
+print("He's got %s eyes and %s hair ." % (usereye,userhair))
+print("His teeth are usually %s depending on the coffee."% userteeth)
