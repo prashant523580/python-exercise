54f3c45287eb26f031922115a23f34da674cca0d
prashant523580 <prashant.thapa62758@gmail.com>
2020-12-22 11:43:47 +0545

    Python Asking question and printing program

---
+++ asking_que.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = input("Age: ")
+print("How tall are you?")
+height = input("1nch: : ")
+print("How much do your weigh? ")
+weight = input("lbs: ")
+
+print("So, you're %r old, %r tall and %r heavy. " % (age,height,weight))
